7847c925c5079914c851aded4d0f1e65b46b6faa
Fatih ŞENER <48569471+FatihSener@users.noreply.github.com>
2019-03-30 17:00:07 +0300

    Initial commit

---
+++ havaYolları/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using havaYollarıKutuphanesi;
+
+namespace sprin2_havaYolları
+{
+    class Program
+    {
+        static string[] koltuklar = new string[20];
+        const int businessClassKoltukSayisi = 8;
+        const int economyClassKoltukSayisi = 12;
+
+        static void Main(string[] args)
+        {
+            //karşılama ekranı yap
+            //tuşa basıldıgında işlem yap.klavyeden okuma işlemi yap.1 mi basıldı, 2mi basıldı
+            //1 veya 2 dışında girince yok say ve ilk gelen ekran açık kalsın
+
+            //business seçilirse boş koltuklar 1 den 8 e kadar listelendsin 1 ve 8 dahil
+            //boş koltuk yoksa boş koltuk yok mesajı döner ve economy sınıfından devam etmek ister misin diye sorulur.
+            //evet derse economy sınıfındaki koltuklar göüntülenir.
+            //hayır derse bir sonraki uçuş 4 saat sonra mesajı alır ve karşılama ekranı geri gelir.
+
+            //seçilen koltuk daha önceden seçildiys o koltuk kime aitse onun adı yazar.
+            //tekrar boş koltuklar görüntülenir ve koltuk numarası seçilir
+            //boş koltuk varsa kullanıcı boş koltk numarasını yazar ve enter'a basar.boş bir koltuk seçilirse kullanıcıdan yolcu ismi alınır.
+            //yolcu ismi alındıktan sonra enter a basınca hangi class tan,hangi koltugun hangi yolcuya verildiği bilgisi ekrana yazılır.
+            //kullanıcı bir tuşa bastıgında program geri döner ve sınıf seçenekleri tekrar görüntülenir.
+
+            Helper.EkranaYaz("***********************************\nRohan Hava Yollarına Hoşgeldiniz\n***********************************");
+            Helper.SatirBasiYap();
+
+            do
+            {
+                RezervasyonUygulaması();
+
+            } while (true);
+
+        }
+
+        static void RezervasyonUygulaması()
+        {
+            string sinif = SinifSec();
+            bool rezerveEdildiMi;
+            int koltukNo;
+            do
+            {
+                string secim = "";
+                do
+                {
+                    secim = "";
+                    koltukNo = BosKoltuklariListele(sinif);
+                    if (koltukNo == -1)
+                    {
+                        if (sinif == "business")
+                        {
+
+                            Helper.EkranaYaz("Economy Class bölümünde boş koltukları görmek ister misiniz? (E/H)");
+                            secim = Console.ReadKey().KeyChar.ToString();
+                            if (secim.ToLower() == "e")
+                            {
+                                sinif = "economy";
+                            }
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Business Class bölümünde boş koltukları görmek ister misiniz? (E/H)");
+                            secim = Console.ReadKey().KeyChar.ToString();
+                            if (secim.ToLower() == "e")
+                            {
+                                sinif = "business";
+                            }
+                        }
+                    }
+                    else if (!String.IsNullOrEmpty(koltuklar[koltukNo - 1]))
+                    {
+                        if (sinif == "business")
+                        {
+                            Console.WriteLine("Business Class bölümündeki {0} numaralı koltuğu daha önce {1} isimli yolcuya rezerve ettiniz\nLütfen boş koltuklardan birini seçiniz.", koltukNo, koltuklar[koltukNo - 1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Economy Class bölümündeki {0} numaralı koltuğu daha önce {1} isimli yolcuya rezerve ettiniz\nLütfen boş koltuklardan birini seçiniz.", koltukNo, koltuklar[koltukNo - 1]);
+                        }
+                    }
+                } while ((koltukNo == -1 && secim.ToLower() == "e") || (koltukNo >= 0 && !String.IsNullOrEmpty(koltuklar[koltukNo - 1])));
+                if (secim.ToLower() == "h")
+                {
+                    Console.WriteLine("Bir sonraki uçuşlar 4 saat sonradır.");
+                    return;
+                }
+                string yolcuAdi = YolcununAdiniAl(koltukNo);
+                rezerveEdildiMi = RezerveEt(sinif, koltukNo, yolcuAdi);
+                if (rezerveEdildiMi)
+                {
+                    Console.WriteLine("Devam etmek için bir tuşa basınız.");
+                    Console.ReadKey(true);
+                }
+            } while (rezerveEdildiMi == false);
+        }
+
+        static string SinifSec()
+        {
+            string tus = "";
+            Helper.EkranaYaz("1. Business Class bölümü için 1 tuşuna basın.");
+            Helper.EkranaYaz("2. Economy Class bölümü için 2 tuşuna basın.");
+
+            do
+            {
+                tus = Console.ReadKey(true).KeyChar.ToString();
+            } while (tus != "1" && tus != "2");
+
+            if (tus == "1")
+            {
+                return "business";
+            }
+            else
+            {
+                return "economy";
+            }
+
+        }
+
+        static string YolcununAdiniAl(int koltukNo)
+        {
+
+            Console.WriteLine("Seçilen koltuk numarası: {0}", koltukNo);
+            Helper.EkranaYaz("Lütfen yolcunun Adını ve Soyadını giriniz: ");
+            return Helper.EkrandanOku();
+        }
+
+        static int BosKoltuklariListele(string sinif)
+        {
+            if (sinif == "business")
+            {
+                bool bosKoltukVarMi = false;
+                Helper.EkranaYaz("Business Class bölümünde kalan boş koltuklar:");
+                for (int i = 0; i < businessClassKoltukSayisi; i++)
+                {
+                    if (String.IsNullOrEmpty(koltuklar[i]))
+                    {
+                        Console.WriteLine("- {0}", i + 1);
+                        bosKoltukVarMi = true;
+                    }
+                }
+
+                Helper.SatirBasiYap();
+
+                if (bosKoltukVarMi == false)
+                {
+
+                    Helper.EkranaYaz("Seçtiğiniz Business Class bölümünde boş koltuk kalmamıştır.");
+                    return -1;
+                }
+            }
+            else if (sinif == "economy")
+            {
+                bool bosKoltukVarMi = false;
+                Helper.EkranaYaz("Economy Class bölümünde kalan boş koltuklar:");
+                for (int i = businessClassKoltukSayisi; i < koltuklar.Length; i++)
+                {
+                    if (String.IsNullOrEmpty(koltuklar[i]))
+                    {
+                        Console.WriteLine("- {0}", i + 1);
+                        bosKoltukVarMi = true;
+                    }
+                }
+                if (bosKoltukVarMi == false)
+                {
+
+                    Helper.EkranaYaz("Seçtiğiniz Economy Class bölümünde boş koltuk kalmamıştır.");
+                    return -1;
+                }
+            }
+            string koltukNoDışardan = "";
+            bool sayiMi = false;
+            int koltukNo;
+            do
+            {
+                koltukNoDışardan = Console.ReadLine();
+                sayiMi = Int32.TryParse(koltukNoDışardan, out koltukNo);
+            } while (sayiMi == false);
+            return koltukNo;
+        }
+
+        static bool RezerveEt(string sinif, int koltukNo, string yolcuAdi)
+        {
+            if (sinif == "business" && koltukNo > 0 && koltukNo <= businessClassKoltukSayisi)
+            {
+                if (String.IsNullOrEmpty(koltuklar[koltukNo - 1]))
+                {
+                    koltuklar[koltukNo - 1] = yolcuAdi;
+                    Console.WriteLine("Business Class bölümündeki {0} numaralı koltuğu {1} isimli yolcuya rezerve ettiniz.", koltukNo, yolcuAdi);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("{0} numaralı koltuğu daha önce \"{1}\" adlı kişiye rezerve ettiniz.", koltukNo, koltuklar[koltukNo - 1]);
+                    Helper.EkranaYaz("Lütfen boş koltuklardan birisini seçiniz.");
+                    return false;
+                }
+            }
+            else if (sinif == "economy" && koltukNo > businessClassKoltukSayisi && koltukNo <= koltuklar.Length)
+            {
+                if (String.IsNullOrEmpty(koltuklar[koltukNo - 1]))
+                {
+                    koltuklar[koltukNo - 1] = yolcuAdi;
+                    Console.WriteLine("Economy Class bölümündeki {0} numaralı koltuğu {1} isimli yolcuya rezerve ettiniz.", koltukNo, yolcuAdi);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("{0} numaralı koltuğu daha önce \"{1}\" adlı kişiye rezerve ettiniz.", koltukNo, koltuklar[koltukNo - 1]);
+                    Helper.EkranaYaz("Lütfen boş koltuklardan birisini seçiniz.");
+                    return false;
+                }
+            }
+            else
+            {
+                Helper.EkranaYaz("Geçersiz sınıf veya koltuk numarası girildi. Lütfen tekrar deneyiniz.");
+                return false;
+            }
+        }
+
+
+
+    }
+}
---
+++ havaYollarıKutuphanesi/helper.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace havaYollarıKutuphanesi
+{
+    public class Helper
+    {
+        #region Framework
+        public static string[] captchaMetinleri = { "AlAForTanFoni", "AlaFor12", "MusTaFa", "CrazyBoYY", "BeLaLIM93" };
+        public static int KontrolluRakamRakamSayiAl(int rakamAdeti, string istekMesaji)
+        {
+            EkranaYaz(istekMesaji);
+            string sayi = "";
+            for (int i = 0; i < rakamAdeti; i++)
+            {
+                int rakam = KontrolluRakamAl();
+                if (rakamAdeti > 1 && i == 0 && rakam == 0)
+                {
+                    i--;
+                    continue;
+                }
+                sayi += rakam;
+            }
+            return int.Parse(sayi);
+        }
+        public static int KontrolluRakamAl()
+        {
+            char karakter = KlavyedenKarakterOku();
+            if (karakter == 13)
+            {
+                return -1;
+            }
+            int sayi;
+            bool donustuMu = int.TryParse(karakter.ToString(), out sayi);
+            if (!donustuMu)
+            {
+                return KontrolluRakamAl();
+            }
+            EkranaYaz("*");
+            return sayi;
+        }
+        public static char KlavyedenKarakterOku()
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            return keyInfo.KeyChar;
+        }
+        public static void EkraniTemizle()
+        {
+            Console.Clear();
+        }
+
+        public static bool BoolSorusu(string soruMetni, string trueSecici, string falseSecici)
+        {
+            EkranaYaz(soruMetni + "(" + trueSecici + "/" + falseSecici + ")....");
+            string input = EkrandanOku();
+            if (input.ToLower() == trueSecici.ToLower())
+            {
+                return true;
+            }
+            else if (input.ToLower() == falseSecici.ToLower())
+            {
+                return false;
+            }
+            else
+            {
+                EkranaYaz("Hatalı bir giriş yaptınız.");
+                SatirBasiYap();
+                return BoolSorusu(soruMetni, trueSecici, falseSecici);
+            }
+        }
+
+        public static int RastgeleSayiSec(int[] sayilar)
+        {
+            Random random = new Random();
+            int indis = random.Next(0, sayilar.Length);
+            return sayilar[indis];
+        }
+
+        public static void Bekle()
+        {
+            Console.ReadKey();
+        }
+
+        public static void EkranaYaz(int sayi)
+        {
+            EkranaYaz(sayi.ToString());
+        }
+
+        public static void EkranaYaz(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+
+        public static int OrtalamaHesapla(int[] sayilar)
+        {
+            int toplam = Topla(sayilar);
+            return toplam / sayilar.Length;
+        }
+
+        public static int Topla(int[] sayilar)
+        {
+            int toplam = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                toplam += sayilar[i];
+            }
+            return toplam;
+        }
+
+        public static void EkranaYaz(int[] sayilar)
+        {
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                EkranaYaz(sayilar[i] + " ");
+            }
+        }
+
+        public static int[] RastgeleSayiUret(int sayiAdeti, int sayiAltSiniri, int sayiUstSiniri)
+        {
+            Random random = new Random();
+            int[] sayilar = new int[sayiAdeti];
+            for (int i = 0; i < sayiAdeti; i++)
+            {
+                sayilar[i] = random.Next(sayiAltSiniri, sayiUstSiniri);
+            }
+            return sayilar;
+        }
+
+        public static int KontrolluSayiAl(string istekMesaji, string hataMesaji)
+        {
+            EkranaYaz(istekMesaji);
+            string input = EkrandanOku();
+            int sayi;
+            bool donustuMu = int.TryParse(input, out sayi);
+            if (donustuMu)
+            {
+                return sayi;
+            }
+            EkranaYaz(hataMesaji);
+            SatirBasiYap();
+            return KontrolluSayiAl(istekMesaji, hataMesaji);
+        }
+
+        public static void SatirBasiYap()
+        {
+            Console.WriteLine();
+        }
+
+        public static string EkrandanOku()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void KarsilamaYap()
+        {
+            EkranaYaz("Rastgele Sayi Uygulamasına Hoş Geldiniz.");
+            SatirBasiYap();
+        }
+
+        public static void DiziyeElemanEkle(ref string[,] dizi, string birinciBoyutDegeri, string ikinciBoyutDegeri)
+        {
+            string[,] yeniDizi = new string[dizi.GetLength(0) + 1, dizi.GetLength(1)];
+            DiziKopyala(dizi, yeniDizi);
+
+            yeniDizi[dizi.GetLength(0), 0] = birinciBoyutDegeri;
+            yeniDizi[dizi.GetLength(0), 1] = ikinciBoyutDegeri;
+
+            dizi = yeniDizi;
+        }
+
+        public static void DiziKopyala(string[,] kaynakDizi, string[,] hedefDizi)
+        {
+            for (int i = 0; i < kaynakDizi.GetLength(0); i++)
+            {
+                for (int j = 0; j < kaynakDizi.GetLength(1); j++)
+                {
+                    hedefDizi[i, j] = kaynakDizi[i, j];
+                }
+            }
+        }
+
+        public static string KontrolluRakamRakamSayiAl(int minRakamAdeti, string istekMesaji, string minSayiHataMesaji)
+        {
+            EkranaYaz(istekMesaji);
+            string sayi = "";
+            do
+            {
+                int rakam = KontrolluRakamAl();
+                if (rakam == -1)
+                {
+                    break;
+                }
+                sayi += rakam;
+
+            } while (true);
+
+            if (sayi.Length < minRakamAdeti)
+            {
+                SatirBasiYap();
+                EkranaYaz(minSayiHataMesaji);
+                SatirBasiYap();
+                return KontrolluRakamRakamSayiAl(minRakamAdeti, istekMesaji, minSayiHataMesaji);
+            }
+            return sayi;
+        }
+
+        public static string KontrolluMetinAl(string istekMesaji, string hataMesaji)
+        {
+            EkranaYaz(istekMesaji);
+            string input = EkrandanOku();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                SatirBasiYap();
+                EkranaYaz(hataMesaji);
+                SatirBasiYap();
+                return KontrolluMetinAl(istekMesaji, hataMesaji);
+            }
+            return input;
+        }
+
+
+        public static void UygulamayiKapat()
+        {
+            Environment.Exit(0);
+        }
+        #endregion
+    }
+}
